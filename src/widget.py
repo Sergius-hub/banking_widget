58e737e174495a89f237aeebb3d248ba432369b7
Sergius-hub <sergiusbox@mail.ru>
2026-01-23 20:17:53 +0300

    добавлена обработка ошибок, исправлено оформление кода, проверка линтерами

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,4 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
+from .masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_or_card: str) -> str:
@@ -9,48 +8,52 @@ def mask_account_card(account_or_card: str) -> str:
     :param account_or_card: Visa Platinum 7000792289606361
     :return masked string: Visa Platinum 7000 79** **** 6361
     """
-    result = ""
-    size_str = len(account_or_card)
-    # Проверяем длину строки
-    if size_str < 16:
-        return "Ошибка: недостаточно символов"
+    try:
 
-    # Определяем позицию среза с конца строки
-    offset_reverse = -16
+        result = ""
+        size_str = len(account_or_card)
+        # Проверяем длину строки
+        if size_str < 16:
+            raise ValueError("Ошибка: недостаточно символов")
 
-    # Полагаем что это номер карты
-    card_number = account_or_card[offset_reverse:]
+        # Определяем позицию среза с конца строки
+        offset_reverse = -16
 
-    # Проверяем номер карты число?
-    if not card_number.isdigit():
-        return "Ошибка: Неправильный номер карты или счёта"
+        # Полагаем что это номер карты
+        card_number = account_or_card[offset_reverse:]
 
-    # Проверяем строку на 20 и более символов
-    if size_str >= 20:
-        # Если строка более 20 можем взять срез, и полагаем что это префикс для номера счета
-        account_number_prefix = account_or_card[offset_reverse - 4 : offset_reverse]
-        # Если префикс число, это номер счета
-        if account_number_prefix.isdigit():
-            # Если цифр достаточно для номера счета уменьшаем офсет
-            offset_reverse -= 4
+        # Проверяем номер карты число?
+        if not card_number.isdigit():
+            raise ValueError("Ошибка: неправильный номер карты или счёта")
 
-    # Проверяем пробел
-    if account_or_card[offset_reverse - 1] != " ":
+        # Проверяем строку на 20 и более символов
+        if size_str >= 20:
+            # Если строка более 20 можем взять срез, и полагаем что это префикс для номера счета
+            account_number_prefix = account_or_card[offset_reverse - 4 : offset_reverse]
+            # Если префикс число, это номер счета
+            if account_number_prefix.isdigit():
+                # Если цифр достаточно для номера счета уменьшаем офсет
+                offset_reverse -= 4
+
+        # Проверяем пробел
+        if account_or_card[offset_reverse - 1] != " ":
+            if offset_reverse == -16:
+                raise ValueError("Ошибка: нет пробела для разделения номера карты")
+            elif offset_reverse == -20:
+                raise ValueError("Ошибка: нет пробела для разделения номера счета")
+
+    except ValueError as e:
+        # Возврат ошибки
+        return str(e)
+
+    else:
+        # Строим результирующую строку
         if offset_reverse == -16:
-            return "Ошибка: нет пробела для разделения номера карты"
+            result = account_or_card[:offset_reverse] + get_mask_card_number(int(card_number))
         elif offset_reverse == -20:
-            return "Ошибка: нет пробела для разделения номера счета"
+            result = account_or_card[:offset_reverse] + get_mask_account(int(account_number_prefix + card_number))
 
-    # Строим результирующую строку
-    if offset_reverse == -16:
-        result = account_or_card[:offset_reverse] + get_mask_card_number(int(card_number))
-    elif offset_reverse == -20:
-        result = account_or_card[:offset_reverse] + get_mask_account(int(account_number_prefix + card_number))
-
-    return result
-
-
-# print(mask_account_card("Счет 73654108430135874305"))
+        return result
 
 
 def get_date(str_date: str) -> str:
@@ -60,21 +63,27 @@ def get_date(str_date: str) -> str:
     :param str_date: "2024-03-11T02:26:18.671407"
     :return: string date: "11.03.2024"
     """
-    result = ""
-    # Проверяем длину строки
-    if len(str_date) < 10:
-        return "Ошибка: недостаточно символов"
+    try:
+        result = ""
+        # Проверяем длину строки
+        if len(str_date) < 10:
+            raise ValueError("Ошибка: недостаточно символов")
 
-    # Получаем дату, месяц, год
-    day_from_date = str_date[8:10]
-    month_from_date = str_date[5:7]
-    year_from_date = str_date[:4]
+        # Получаем дату, месяц, год
+        day_from_date = str_date[8:10]
+        month_from_date = str_date[5:7]
+        year_from_date = str_date[:4]
 
-    # Если хотя бы одно из значений не число, возвращаем ошибку
-    if not day_from_date.isdigit() or not month_from_date.isdigit() or not year_from_date.isdigit():
-        return "Ошибка: некорректная дата"
+        # Если хотя бы одно из значений не число, возвращаем ошибку
+        if not day_from_date.isdigit() or not month_from_date.isdigit() or not year_from_date.isdigit():
+            raise ValueError("Ошибка: некорректная дата")
 
-    # Формируем дату в нужном формате
-    result = f"{day_from_date}.{month_from_date}.{year_from_date}"
+    except ValueError as e:
+        # Возврат ошибки
+        return str(e)
 
-    return result
+    else:
+        # Формируем дату в нужном формате
+        result = f"{day_from_date}.{month_from_date}.{year_from_date}"
+
+        return result
