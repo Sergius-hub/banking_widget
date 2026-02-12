c9c103c27be4408c9a093b5a2ac60c7150425dde
Sergius <sergiusbox@mail.ru>
2026-02-12 12:45:22 +0300

    тест модуля masks.py, исправление проблем проблем в соответствии с тестами

--- src/masks.py
+++ src/masks.py
@@ -4,9 +4,10 @@ def get_mask_card_number(card_number: int) -> str:
     # Проверяем номер на соответствие типа аргумента, должен быть int, если нет выбрасываем исключение
     if not isinstance(card_number, int):
         raise TypeError("Ошибка: аргумент не целое число")
-
+    # Проверка на отрицательность, если отрицательное то выбрасываем исключение
     if card_number < 0:
         raise ValueError("Ошибка: аргумент не может быть отрицательным")
+
     # Приводим число к строке
     card_number_str = str(card_number)
 
@@ -21,30 +22,26 @@ def get_mask_card_number(card_number: int) -> str:
     # Вывод: замаскированой строки
     return formatted_mask
 
-# print(len(str(-123456789012345)))
 
 def get_mask_account(account_number: int) -> str:
     """Функция принимает номер карты, возвращает строку с замаскированными цифрами номера карты"""
 
-    # Ожидаем ошибку
-    try:
-        # Проверяем номер на соответствие типа аргумента, должен быть int
-        if type(account_number).__name__ != "int":
-            raise TypeError("Ошибка: неправильный тип аргумента в функции 'get_mask_account()'")
-        # Приводим число к строке
-        account_number_str = str(account_number)
+    # Проверяем номер на соответствие типа аргумента, должен быть int, если нет выбрасываем исключение
+    if not isinstance(account_number, int):
+        raise TypeError("Ошибка: аргумент не целое число")
+    # Проверка на отрицательность, если отрицательное то выбрасываем исключение
+    if account_number < 0:
+        raise ValueError("Ошибка: аргумент не может быть отрицательным")
 
-        # Проверяем количество символов (цифр числа), должно быть 20
-        if len(account_number_str) != 20:
-            raise ValueError("Ошибка: номер карты не соответствует 20-ти цифрам")
+    # Приводим число к строке
+    account_number_str = str(account_number)
 
-    # Блок обработки ошибок
-    except (TypeError, ValueError) as e:
-        # Возвращаем ошибку
-        return str(e)
-    else:
-        # Производим маскировку
-        mask = f"**{account_number_str[-4:]}"
+    # Проверяем количество символов (цифр числа), должно быть 20
+    if len(account_number_str) != 20:
+        raise ValueError("Ошибка: номер карты не соответствует 20-ти цифрам")
 
-        # Вывод: замаскированой строки
-        return mask
+    # Производим маскировку
+    mask = f"**{account_number_str[-4:]}"
+
+    # Вывод: замаскированой строки
+    return mask
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,51 +2,75 @@ import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
-# 1. КОНСТАНТЫ С ТЕСТОВЫМИ ДАННЫМИ
-SUCCESS_CASES = [
+# 1. КОНСТАНТЫ С ТЕСТОВЫМИ ДАННЫМИ ДЛЯ НОМЕРА КАРТЫ
+SUCCESS_CASES_CARD_NUMBERS = [
     (7000792289606361, "7000 79** **** 6361"),
     (1234567890123456, "1234 56** **** 3456"),
     (1111222233334444, "1111 22** **** 4444"),
     (8888888888888888, "8888 88** **** 8888")
 ]
 
-ERROR_CASES_TYPE = [
+ERROR_CASES_TYPE_CARD_NUMBERS = [
     "7000792289606361",  # строка
     "1234-5678-9012",    # форматированная строка
     "",                  # пустая строка
     None,                # None
 ]
 
-ERROR_CASES_VALUE = [
+ERROR_CASES_VALUE_CARD_NUMBERS = [
     1234,               # слишком короткий
     -123456789012345,   # отрицательный
     0,                  # ноль
 ]
 
 # Тестируем УСПЕШНЫЕ сценарии функции get_mask_card_number(card_number)
-@pytest.mark.parametrize("card_number, expected_result", SUCCESS_CASES)
+@pytest.mark.parametrize("card_number, expected_result", SUCCESS_CASES_CARD_NUMBERS)
 def test_get_mask_card_number_success(card_number, expected_result):
     assert get_mask_card_number(card_number) == expected_result
 
 # Тестируем ошибки типов в аргументах функции get_mask_card_number(card_number)
-@pytest.mark.parametrize("invalid_type", ERROR_CASES_TYPE)
+@pytest.mark.parametrize("invalid_type", ERROR_CASES_TYPE_CARD_NUMBERS)
 def test_get_mask_card_number_type_errors(invalid_type):
     with pytest.raises(TypeError):
         get_mask_card_number(invalid_type)
 
 # Тестируем ошибки значений в аргументах функции get_mask_card_number(card_number)
-@pytest.mark.parametrize("invalid_value", ERROR_CASES_VALUE)
+@pytest.mark.parametrize("invalid_value", ERROR_CASES_VALUE_CARD_NUMBERS)
 def test_get_mask_card_number_value_errors(invalid_value):
     with pytest.raises(ValueError):
         get_mask_card_number(invalid_value)
 
+# 2. КОНСТАНТЫ С ТЕСТОВЫМИ ДАННЫМИ ДЛЯ НОМЕРА СЧЕТА
+SUCCESS_CASES_ACCOUNT_NUMBERS = [
+    (70007922896063611234, "**1234"),
+    (12345678901212343456, "**3456"),
+    (11112222333312344444, "**4444"),
+    (88888888888812348888, "**8888")
+]
 
-# # Тестируем функцию get_mask_account(account_number)
-# @pytest.mark.parametrize(
-#     "account_number, expected_result",
-#     [
-#         (73654108430135874305, "**4305")
-#     ]
-# )
-# def test_get_mask_account(account_number, expected_result):
-#     assert get_mask_account(account_number) == expected_result
+ERROR_CASES_TYPE_ACCOUNT_NUMBERS = [
+    "70007922896063611234",  # строка
+    "1234-5678-9012-9999",    # форматированная строка
+    "",                  # пустая строка
+    None,                # None
+]
+
+ERROR_CASES_VALUE_ACCOUNT_NUMBERS = [
+    1234,               # слишком короткий
+    -1234567890123456789,   # отрицательный
+    0,                  # ноль
+]
+
+@pytest.mark.parametrize("account_number, expected_result", SUCCESS_CASES_ACCOUNT_NUMBERS)
+def test_get_mask_account_success(account_number, expected_result):
+    assert get_mask_account(account_number) == expected_result
+
+@pytest.mark.parametrize("invalid_type", ERROR_CASES_TYPE_ACCOUNT_NUMBERS)
+def test_get_mask_account_type_errors(invalid_type):
+    with pytest.raises(TypeError):
+        get_mask_account(invalid_type)
+
+@pytest.mark.parametrize("invalid_value", ERROR_CASES_VALUE_ACCOUNT_NUMBERS)
+def test_get_mask_account_type_errors(invalid_value):
+    with pytest.raises(ValueError):
+        get_mask_account(invalid_value)
